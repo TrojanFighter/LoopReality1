e6c1f7f66d61ca61b022fcbdcaa93593f74b3a1c
Sparky <timdao@hotmail.com>
2018-04-30 02:48:07 -0400

    Cursor Moving

--- Assets/Main/Scripts/MouseInteractions/VirtualMouse.cs
+++ Assets/Main/Scripts/MouseInteractions/VirtualMouse.cs
@@ -7,11 +7,21 @@ public class VirtualMouse : MonoBehaviour
 
     public Transform m_RestorePosition;
     public MouseBody m_MouseBody;
-    
+    public Vector3 m_lastPosition,deltaPosition;
+    public ScreenCursor m_cursor;
+
+    void Start()
+    {
+        m_lastPosition = transform.position;
+    }
+
     public void MoveToPosition(Vector3 position)
     {
         //position=new Vector3(position.x,m_RestorePosition.position.y,position.z);
         transform.position =position;
+        deltaPosition = position - m_lastPosition;
+        m_cursor.DeltaMove(deltaPosition);
+        m_lastPosition = position;
     }
 
     public void RestorePosition()
---
+++ Assets/Main/Scripts/Screen/ScreenCursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCursor : MonoBehaviour
+{
+    private Rigidbody m_rigidbody;
+
+    private void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void DeltaMove(Vector3 delta)
+    {
+        //delta=new Vector3(delta.x,delta.z,delta.y);
+        m_rigidbody.position += delta;
+    }
+}
